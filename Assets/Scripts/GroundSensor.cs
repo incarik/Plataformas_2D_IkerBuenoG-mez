297637f17faa143d7078abf3500790e6a9ddc666
incarik <incarik1@gmail.com>
2024-09-20 13:18:13 +0200

    animaciones y camara

---
+++ Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    public static bool isGrounded;
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        isGrounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.layer == 6)
+        {
+            isGrounded = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerConroller.cs
+++ Assets/Scripts/PlayerConroller.cs
@@ -24,9 +24,19 @@ public class PlayerConroller : MonoBehaviour
     {
        horizontalInput = Input.GetAxis("Horizontal"); 
 
-       if(Input.GetButtonDown("Jump"))
+       if(horizontalInput < 0)
        {
-         characterRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            transform.rotation = Quaternion.Euler(0, 180, 0); //sirve para girar al personaje de una manera compleja
+       }
+
+       else if(horizontalInput > 0)
+       {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+       }
+      
+       if(Input.GetButtonDown("Jump") && GroundSensor.isGrounded)
+       {
+         characterRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); 
        }
     }
 
